9df8530352459f3c9e1ace077d766193230310cf
gakki-oss <j08135593@gmail.com>
2026-01-21 23:55:23 +0800

    修改

--- leetcode1109_booking_Fly.cpp
+++ leetcode1109_booking_Fly.cpp
@@ -30,7 +30,7 @@ public:
             diff[booking[0] - 1] += booking[2];
             if(booking[1]<n)
             {
-                diff[booking[1] + 1] -= booking[2];
+                diff[booking[1]] -= booking[2];
             }
         }
         for (int i = 1; i < diff.size();i++)
